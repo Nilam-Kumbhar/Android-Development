ef3747d506f3046201a10423d258c4307934fcb8
Nilam-Kumbhar <nilamkumbhar1605@gmail.com>
2026-05-04 12:00:00 +0530

    Resolved merge conflict

--- GridLayout/app/src/main/java/com/example/gridlayout/MainActivity.java
+++ GridLayout/app/src/main/java/com/example/gridlayout/MainActivity.java
@@ -1,16 +1,11 @@
 package com.example.gridlayout;
-<<<<<<< HEAD
-//grid layout
-=======
 
->>>>>>> 235dcf6bb16995950b14009204e3d22de685bbc7
+// grid layout
+
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +14,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
     }
 }
